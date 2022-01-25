459ec4d05701a0d771d6c759194d592816d64d2c
achechet <achechet@gmail.com>
2022-01-25 18:35:53 +0300

    Lesson-6

--- chechetkin/travels/views.py
+++ chechetkin/travels/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 
+from .models import *
+
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+
 def index(request):
-    if (request.GET):
-        print(request.GET)
-        
-    return HttpResponse("Страница приложения Travels")
+    posts = Travel.objects.all()
+    return render(request, 'travels/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+
+def about(request):
+    return render(request, 'travels/about.html', {'menu': menu,'title': 'О сайте'})
 
 def categories(request, catid):
     return HttpResponse(f"<h2>Список статей по странам</h2><p>{catid}</p>")
