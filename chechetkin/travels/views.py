4a4c4701cfae7f4b5dca59e3f8f442a853fde025
achechet <achechet@gmail.com>
2022-01-26 16:59:27 +0300

    Lesson-8

--- chechetkin/travels/views.py
+++ chechetkin/travels/views.py
@@ -3,17 +3,36 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404
 
 from .models import *
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'},
+        ]
 
 def index(request):
     posts = Travel.objects.all()
-    return render(request, 'travels/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+    context = {
+        'posts': posts,
+        'menu': menu, 
+        'title': 'Главная страница'      
+    }
+    
+    return render(request, 'travels/index.html', context=context)
 
 def about(request):
     return render(request, 'travels/about.html', {'menu': menu,'title': 'О сайте'})
 
-def categories(request, catid):
-    return HttpResponse(f"<h2>Список статей по странам</h2><p>{catid}</p>")
+def addpage(request):
+    return HttpResponse("Добавление статьи")
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+def login(request):
+    return HttpResponse("Авторизация")
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
 
 def archive(request, year):
     if int(year) > 2020:
