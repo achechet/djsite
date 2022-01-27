52dde4897fe3a41b05449738dd786ad7c77c7bfe
achechet <achechet@gmail.com>
2022-01-27 12:12:57 +0300

    Lesson-10

--- chechetkin/travels/apps.py
+++ chechetkin/travels/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class TravelsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'travels'
+    verbose_name = 'Путешествия'
--- chechetkin/travels/models.py
+++ chechetkin/travels/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.urls import reverse
 
 class Travel(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
     content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
-    time_create = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фотография")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
     cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
@@ -15,13 +15,23 @@ class Travel(models.Model):
     
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
+    
+    class Meta:
+        verbose_name = 'Путешествия'
+        verbose_name_plural = 'Путешествия'
+        ordering = ['-time_create', 'title']
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name="Категория")
     
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_id': self.pk})
+    
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['id',]        
         
